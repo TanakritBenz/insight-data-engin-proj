bb732f5c68041c1eb8e627287c5363a056664c60
Tanakrit Supanaraphan <banziilloic@gmail.com>
2017-02-05 22:07:30 -0800

    New way to write to Cassandra in parallel

---
+++ spark/posts-processing.py
@@ -0,0 +1,119 @@
+from __future__ import print_function
+import sys, json, copy, time, pprint, datetime
+import pyspark_cassandra
+from cassandra import ConsistencyLevel
+from pyspark import SparkConf, SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from elasticsearch import Elasticsearch
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from config import ES_HOST, SPARK_MASTER, KAFKA_BROKER_1, KAFKA_BROKER_2, KAFKA_BROKER_3
+
+
+def expandRDDbyMatch(x):
+    res = []
+    for match in x[1]['matches']:
+        tmp_dict = copy.copy(x[1])
+        tmp_dict['query'] = match[u'_id']
+        res.append((x[0], tmp_dict))
+    return res
+
+def limitMatchesAt5(x):
+    if x[1]['match_total'] > 5:
+        x[1]['match_total'] = 5
+        x[1]['matches'] = x[1]['matches'][:5]
+    return x
+
+def percolatePost(x):
+    es = Elasticsearch([ES_HOST])
+    tmp_doc = {'doc': {'post': x[1]['title']}}
+    percol_res = es.percolate(index="post_percolators", doc_type="doctype", body=tmp_doc)
+    x[1]['percol_res'] = percol_res
+    try:
+        x[1]['match_total'] = x[1]['percol_res']['total']
+        x[1]['matches'] = x[1]['percol_res']['matches']
+        del x[1]['percol_res']
+    except KeyError:
+        pass
+    return x
+
+def formatForCassandraPostsTable(x):
+    x = {
+        'query': x[1]['query'],
+        'created_utc': datetime.datetime.fromtimestamp(x[1]['created_utc']),
+        'doc_id': x[0],
+        'subreddit': x[1]['subreddit'],
+    }
+    return x
+
+def formatForCassandraDocsTable(x):
+    x = {
+        'doc_id': x[0],
+        'title': x[1]['title'],
+        'permalink': x[1]['permalink'],
+        'url': x[1]['url'],
+        'author': x[1]['author'],
+        'subreddit': x[1]['subreddit'],
+        'created_utc': datetime.datetime.fromtimestamp(x[1]['created_utc']),
+        'ups': x[1]['ups'],
+        'downs': x[1]['downs'],
+        'gilded': x[1]['gilded'],
+        'score': x[1]['score'],
+    }
+    return x
+
+def processRDD(rdd):
+    start_time = time.time()
+    rdd = (
+        rdd.map(lambda x: (x['id'], x))
+           .reduceByKey(lambda x,y: x) #discard similar posts
+           .filter(lambda x: x[1]['over_18'] == False)
+           .map(percolatePost)
+           .filter(lambda x: x[1]['match_total'] > 0)
+    )
+
+    doc_rdd = rdd.map(formatForCassandraDocsTable)
+    pprint.pprint(doc_rdd.take(3))
+    print('#docs processed => ', doc_rdd.count())
+
+    post_rdd = (
+        rdd.map(limitMatchesAt5)
+           .flatMap(expandRDDbyMatch)
+           .map(formatForCassandraPostsTable)
+    )
+    pprint.pprint(post_rdd.take(3))
+    print('#posts processed =>', post_rdd.count())
+
+    post_rdd.saveToCassandra(
+        keyspace="reddit",
+        table="posts",
+        consistency_level=ConsistencyLevel.ANY,
+        batch_grouping_key='partition',
+        ttl=300000
+    )
+
+    doc_rdd.saveToCassandra(
+        keyspace="reddit",
+        table="docs",
+        consistency_level=ConsistencyLevel.ANY,
+        batch_grouping_key='partition',
+        ttl=300000
+    )
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("Reddit Posts Digest Process").setMaster(SPARK_MASTER)
+    sc = SparkContext(conf=conf)
+    ssc = StreamingContext(sc, 2)
+    brokers = KAFKA_BROKER_1 + ',' + KAFKA_BROKER_2 + ',' + KAFKA_BROKER_3
+    topic = "reddit-posts"
+    kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
+    posts = (
+        kvs.map(lambda x: json.loads(x[1]))
+           .filter(lambda r: len(r) > 0)
+           .foreachRDD(processRDD)
+    )
+
+    ssc.start()
+    ssc.awaitTermination()
